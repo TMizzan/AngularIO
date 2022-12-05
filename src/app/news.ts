96fb1e7c05db4afdb962d2f13c220f520a3ab8d9
TMizzan <terrellrhodes@hotmail.com>
2022-12-05 13:02:20 -0500

    1 file added

---
+++ src/app/news.ts
@@ -0,0 +1,14 @@
+export interface News {
+    author: string;
+    title: string;
+    description: string;
+    url: string;
+    source: string;
+    image: string;
+    category: string;
+    language: string;
+    county: string;
+    published_at: string;
+};
+
+export const news = [];
